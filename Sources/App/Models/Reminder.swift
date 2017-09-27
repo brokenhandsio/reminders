d333a3036f475fd860c795b56462489397949bab
Tim <0xtimc@gmail.com>
2017-09-28 00:56:31 +0100

    Add the relationship between users and reminders

--- Sources/App/Models/Reminder.swift
+++ Sources/App/Models/Reminder.swift
@@ -5,21 +5,25 @@ final class Reminder: Model {
     
     let title: String
     let description: String
+    let user: Identifier?
     
-    init(title: String, description: String) {
+    init(title: String, description: String, userId: Identifier) {
         self.title = title
         self.description = description
+        self.user = userId
     }
     
     init(row: Row) throws {
         title = try row.get("title")
         description = try row.get("description")
+        user = try row.get(User.foreignIdKey)
     }
     
     func makeRow() throws -> Row {
         var row = Row()
         try row.set("title", title)
         try row.set("description", description)
+        try row.set(User.foreignIdKey, user)
         return row
     }
 }
@@ -30,6 +34,7 @@ extension Reminder: Preparation {
             builder.id()
             builder.string("title")
             builder.string("description")
+            builder.parent(User.self)
         }
     }
     
@@ -40,7 +45,7 @@ extension Reminder: Preparation {
 
 extension Reminder: JSONConvertible {
     convenience init(json: JSON) throws {
-        try self.init(title: json.get("title"), description: json.get("description"))
+        try self.init(title: json.get("title"), description: json.get("description"), userId: try json.get("user_id"))
     }
     
     func makeJSON() throws -> JSON {
@@ -48,6 +53,7 @@ extension Reminder: JSONConvertible {
         try json.set("id", id)
         try json.set("title", title)
         try json.set("description", description)
+        try json.set("user_id", user)
         return json
     }
 }
