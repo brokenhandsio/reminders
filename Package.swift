947bfb489768b43915e9d9da6d15b2907787bff9
Tim <0xtimc@gmail.com>
2017-09-27 21:02:54 +0100

    Update Package.swift for Fluent Provider Swift 4 release

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/vapor/vapor.git", .upToNextMajor(from: "2.1.0")),
-        .package(url: "https://github.com/vapor/fluent-provider.git", .upToNextMajor(from: "1.0.0")),
+        .package(url: "https://github.com/vapor/fluent-provider.git", .upToNextMajor(from: "1.2.0")),
     ],
     targets: [
         .target(name: "App", dependencies: ["Vapor", "FluentProvider"],
