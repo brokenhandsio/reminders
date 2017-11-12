d602bd49e56d8f1a18d85625525b01633aebabc8
Tim <0xtimc@gmail.com>
2017-11-12 01:21:23 +0000

    Add pages for users and categories

--- Sources/App/Controllers/WebController.swift
+++ Sources/App/Controllers/WebController.swift
@@ -11,12 +11,18 @@ class WebController {
     func addRoutes(to drop: Droplet) {
         drop.get(handler: indexHandler)
         drop.get("reminders", Reminder.parameter, handler: reminderHandler)
+        drop.get("users", handler: allUsersHandler)
+        drop.get("users", User.parameter, handler: userHandler)
+        drop.get("categories", handler: allCategoriesHandler)
+        drop.get("categories", Category.parameter, handler: categoryHandler)
     }
 
     func indexHandler(_ req: Request) throws -> ResponseRepresentable {
         var parameters: [String: NodeRepresentable] = [:]
         parameters["page_title"] = "Home"
         parameters["reminders"] = try Reminder.all()
+        parameters["users"] = try User.all()
+        parameters["categories"] = try Category.all()
 
         return try viewRenderer.make("index", parameters)
     }
@@ -34,6 +40,50 @@ class WebController {
         }
 
         return try viewRenderer.make("reminder", parameters)
+    }
+
+    func allUsersHandler(_ req: Request) throws -> ResponseRepresentable {
+        var parameters: [String: NodeRepresentable] = [:]
+        parameters["page_title"] = "Users"
+        parameters["users"] = try User.all()
+
+        return try viewRenderer.make("users", parameters)
+    }
+
+    func userHandler(_ req: Request) throws -> ResponseRepresentable {
+        let user = try req.parameters.next(User.self)
+
+        var parameters: [String: NodeRepresentable] = [:]
+        parameters["page_title"] = user.name
+        parameters["user"] = user
+
+        if try user.reminders.count() > 0 {
+            parameters["reminders"] = try user.reminders.all()
+        }
+
+        return try viewRenderer.make("user", parameters)
+    }
+
+    func allCategoriesHandler(_ req: Request) throws -> ResponseRepresentable {
+        var parameters: [String: NodeRepresentable] = [:]
+        parameters["page_title"] = "Categories"
+        parameters["categories"] = try Category.all()
+
+        return try viewRenderer.make("categories", parameters)
+    }
+
+    func categoryHandler(_ req: Request) throws -> ResponseRepresentable {
+        let category = try req.parameters.next(Category.self)
+
+        var parameters: [String: NodeRepresentable] = [:]
+        parameters["page_title"] = category.name
+        parameters["category"] = category
+
+        if try category.reminders.count() > 0 {
+            parameters["reminders"] = try category.reminders.all()
+        }
+
+        return try viewRenderer.make("category", parameters)
     }
 
 }
